104ee091400a26a2721712335c0545b2f1a5c1c0
moguizhizi <53420703+moguizhizi@users.noreply.github.com>
2020-05-07 14:50:01 +0800

    Add files via upload

---
+++ bert_base/bert/__init__.py
@@ -0,0 +1,6 @@
+"""
+
+ @Time    : 2019/1/30 19:09
+ @Author  : MaCan (ma_cancan@163.com)
+ @File    : __init__.py.py
+"""
---
+++ bert_base/train/common.py
@@ -0,0 +1,23 @@
+# encoding: utf-8
+"""
+@author: sunkai
+@contact: moguizhiz@126.com
+@time: 2020/4/20 17:39
+@file: common.py
+@desc: 公共常量
+"""
+
+"""默认标签"""
+DEFAUL_LABELS = ["O",  "B-PER", "I-PER", "B-ORG", "I-ORG", "B-LOC", "I-LOC", "X", "[CLS]", "[SEP]"]
+
+"""训练文件"""
+TRAIN_DATA = "train_data"
+
+"""测试文件"""
+TEST_DATA = "test_data"
+
+"""DEV文件"""
+DEV_DATA = "dev_data"
+
+"""标签文件"""
+LABELS_DATA = 'labels.txt'
